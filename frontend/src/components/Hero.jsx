4d661013a3f6de0659544aa9b95beaa0dbb22776
Surya <musurya2014@gmail.com>
2024-07-08 19:38:58 +0530

    Fixed responsiveness of the Hero

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -17,35 +17,37 @@ const HeroSection = () => {
 
   return (
     <div className="relative bg-gradient-to-r from-[#0D47A1] to-[#42A5F5] text-white h-[60vh] flex items-center justify-center z-1">
-      {/* Abstract Geometric Pattern Overlay */}
-      <div className="absolute inset-0 bg-pattern opacity-30"></div>
-      <div className="relative text-center max-w-2xl mx-auto px-4">
-        <h1 className="text-4xl sm:text-5xl font-bold mb-4">
-          Welcome to the VIT Lost & Found Hub
-        </h1>
-        <p className="text-lg sm:text-xl mb-8">
-          Connecting you with your belongings.
-        </p>
-        <div className="md:flex justify-center space-x-4 ">
-          <div className="flex items-center bg-white text-[#0D47A1] px-4 py-2 rounded shadow">
-            <span>I lost </span>
-            <input
-              type="text"
-              placeholder="my belonging "
-              className="ml-2 border-0 outline-none"
-            />
-            <FaSearch className="ml-2" />
-          </div>
+    {/* Abstract Geometric Pattern Overlay */}
+    <div className="absolute inset-0 bg-pattern opacity-30"></div>
+    <div className="relative text-center max-w-2xl mx-auto px-4">
+      <h1 className="text-4xl sm:text-5xl font-bold mb-4">
+        Welcome to the VIT Lost & Found Hub
+      </h1>
+      <p className="text-lg sm:text-xl mb-8">
+        Connecting you with your belongings.
+      </p>
+      <div className="flex flex-wrap justify-center space-x-0 space-y-4 md:space-x-4 md:space-y-0">
+        <div className="flex items-center bg-white text-[#0D47A1] px-4 py-2 rounded shadow w-full md:w-auto">
+          <span>I lost </span>
+          <input
+            type="text"
+            placeholder="my belonging"
+            className="ml-2 border-0 outline-none flex-1"
+          />
+          <FaSearch className="ml-2" />
+        </div>
+        <div className="flex w-full md:w-auto">
           <Link
-            className="bg-white text-[#0D47A1] px-4 py-2 rounded shadow hover:bg-gray-200 transition"
+            className="bg-white text-[#0D47A1] px-4 py-2 rounded shadow w-full md:w-auto hover:bg-gray-200 transition"
             onClick={handleOpenPopup}
           >
             I Found Something
           </Link>
         </div>
       </div>
-      <FormPopup isOpen={showPopup} onClose={handleClosePopup} />
     </div>
+    <FormPopup isOpen={showPopup} onClose={handleClosePopup} />
+  </div>
   );
 };
 
