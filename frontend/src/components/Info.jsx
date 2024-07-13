bb0015acdba24482114a97170e3909c30b1693c6
Shanky <shashanksharma03.07@gmail.com>
2024-07-14 01:24:49 +0530

    modified the styling of the profile page

--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getProfileDataFunction } from "../services/API";
 import ToastMsg from "../constants/ToastMsg";
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner, FaUserCircle } from "react-icons/fa";
 
 export default function Info() {
   const [loading, setLoading] = useState(false);
@@ -57,38 +57,39 @@ export default function Info() {
           Loading...
         </>
       ) : (
-        <div className="w-full md:w-1/2 bg-white py-6 px-4 md:px-10 rounded-lg shadow-md flex flex-wrap lg:flex-nowrap md:flex-col lg:flex-row md:items-center lg:items-start">
+        <div className="w-full md:w-1/2 bg-blue-100 py-6 px-4 md:px-10 rounded-tl-lg rounded-bl-lg flex flex-wrap lg:flex-nowrap md:flex-col lg:flex-row md:items-center lg:items-start">
           <div className="w-full lg:w-1/4 md:mb-0 flex justify-center md:justify-start mb-4">
             <div 
               className="w-32 h-32 rounded-full overflow-hidden border-gray-400">
-              <img
+              {/* <img
                 alt="profilepic"
                 src="https://i1.wp.com/cdn130.picsart.com/344993131001211.png"
                 className="w-full h-full object-cover"
-              />
+              /> */}
+              <FaUserCircle size={"100px"} className="mx-auto text-blue-300" />
             </div>
           </div>
-          <div className="w-full lg:w-3/4 md:py-5 px-4 md:px-6">
+          <div className="w-full lg:w-3/4 md:py-0 px-4 md:px-6">
             <div className="mb-4">
-              <h2 className="text-lg font-bold text-indigo-600">Name:</h2>
-              <p className="text-gray-700 text-base">{userData.name}</p>
+              <h2 className="text-base font-bold text-blue-600">Name:</h2>
+              <p className="text-gray-700 text-[14px]">{userData.name}</p>
             </div>
             <div className="mb-4">
-              <h2 className="text-lg font-bold text-indigo-600">Email:</h2>
-              <p className="text-gray-700 text-base break-words">{userData.email}</p>
+              <h2 className="text-base font-bold text-blue-600">Email:</h2>
+              <p className="text-gray-700 text-[14px] break-words">{userData.email}</p>
             </div>
             <div className="mb-4">
-              <h2 className="text-lg font-bold text-indigo-600">Registration Number:</h2>
-              <p className="text-gray-700 text-base">{userData.registrationNo}</p>
+              <h2 className="text-base font-bold text-blue-600">Registration Number:</h2>
+              <p className="text-gray-700 text-[14px]">{userData.registrationNo}</p>
             </div>
             <div className="mb-4">
-              <h2 className="text-lg font-bold text-indigo-600">Day Scholar/ Hosteler:</h2>
-              <p className="text-gray-700 text-base">{userData.dayScholarOrHosteler}</p>
+              <h2 className="text-base font-bold text-blue-600">Day Scholar/ Hosteler:</h2>
+              <p className="text-gray-700 text-[14px]">{userData.dayScholarOrHosteler}</p>
             </div>
             {userData.phoneNumber && (
               <div className="mb-4">
-                <h2 className="text-lg font-bold text-indigo-600">Phone Number:</h2>
-                <p className="text-gray-700 text-base">{userData.phoneNumber}</p>
+                <h2 className="text-base font-bold text-blue-600">Phone Number:</h2>
+                <p className="text-gray-700 text-[14px]">{userData.phoneNumber}</p>
               </div>
             )}
           </div>
