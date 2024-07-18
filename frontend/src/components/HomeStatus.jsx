35e370fd9328365c930866586b3a916ba6feda0b
Shanky <shashanksharma03.07@gmail.com>
2024-07-18 22:30:41 +0530

    modified home page

--- frontend/src/components/HomeStatus.jsx
+++ frontend/src/components/HomeStatus.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
+import Doughnut from "./DoughnutChart";
 import ToastMsg from "../constants/ToastMsg";
 import { getHomePageGraphDataFunction } from "../services/API";
 import { FaSpinner } from "react-icons/fa";
 
 const HomeStatus = () => {
+  const [profileTab, setProfileTab] = useState("AllUsers");
   const [loading, setLoading] = useState(false);
   const [graphData, setGraphData] = useState({
     allUsersData: {
@@ -40,26 +42,82 @@ const HomeStatus = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Stats</h1>
-      {loading ? (
-        <>
-          <FaSpinner className="mr-2 animate-spin" />
-          Loading...
-        </>
-      ) : (
-        <div className="">
-          <p>
-            No of registered users: {graphData.allUsersData.noOfRegisteredUsers}
-          </p>
-          <p>
-            No of non registered users:{" "}
-            {graphData.allUsersData.noOfNonRegisteredUsers}
-          </p>
-          <p>No of found posts: {graphData.postsData.noOfFoundPosts}</p>
-          <p>No of lost posts: {graphData.postsData.noOfLostPosts}</p>
+    <div className="w-full flex flex-col items-center justify-center gap-4 p-4 md:p-8">
+      <div className="w-full bg-blue-50 p-4 rounded-lg border">
+        <div className="w-full">
+          <nav className="flex justify-center py-2 ">
+            <button
+              className={`p-1 w-full text-center border-b font-medium text-sm 
+                ${
+                  profileTab === "AllUsers" ? ` border-blue-500` : "border-transparent"
+                } `}
+              onClick={() => setProfileTab("AllUsers")}
+            >
+              Users
+            </button>
+            <button
+              className={`p-1 w-full text-center border-b font-medium text-sm 
+                ${
+                  profileTab === "AllPosts"
+                    ? ` border-blue-500`
+                    : "border-transparent"
+                } `}
+              onClick={() => setProfileTab("AllPosts")}
+            >
+              Posts
+            </button>
+          </nav>
+          {loading ? (
+            <>
+              <FaSpinner className="mr-2 animate-spin" />
+              Loading...
+            </>
+          ) : (
+            <div className="mt-4">
+              {profileTab === "AllPosts" && (
+                <div className="py-1">
+                  <Doughnut
+                    labels={["Found Posts", "Lost Posts"]}
+                    dataSet={[
+                      graphData.postsData.noOfFoundPosts,
+                      graphData.postsData.noOfLostPosts,
+                    ]}
+                    backgroundColor={[
+                      "rgba(66, 165, 245, 0.2)",
+                      "rgba(255, 183, 77, 0.2)",
+                    ]}
+                    borderColor={[
+                      "rgba(66, 165, 245, 1)",
+                      "rgba(255, 183, 77, 1)",
+                    ]}
+                  />
+                </div>
+              )}
+              {profileTab === "AllUsers" && (
+                <div className="py-1">
+                  <Doughnut
+                    labels={["Registered Users", "Non Registered Users"]}
+                    dataSet={[
+                      graphData.allUsersData.noOfRegisteredUsers,
+                      graphData.allUsersData.noOfNonRegisteredUsers,
+                    ]}
+                    backgroundColor={[
+                      "rgba(66, 165, 245, 0.2)",
+                      "rgba(255, 183, 77, 0.2)",
+                      // "rgba(205,0,0,0.3)",
+                    ]}
+                    borderColor={[
+                      "rgba(66, 165, 245, 1)",
+                      "rgba(255, 183, 77, 1)",
+                      // "rgba(205,0,0,0.5)",
+                    ]}
+                  />
+                </div>
+              )}
+            </div>
+          )}
         </div>
-      )}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import HeroSection from "../components/Hero";
-import { ColumnsWithChart, HomeStatus, ItemCarousel } from "../components";
+import { ColumnsWithChart, ItemCarousel } from "../components";
 
 const Home = () => {
   return (
     <div>
       {/* <h1 style={{ textAlign: "center" }}>Home Page!</h1> */}
       <HeroSection />
-      <ColumnsWithChart />
       <ItemCarousel heading={"Recently Found Items"} />
       <ItemCarousel heading={"Recently Lost Items"} />
-      <HomeStatus />
+      <ColumnsWithChart />
     </div>
   );
 };
