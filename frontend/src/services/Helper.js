edf329e8ad5996db59f3012b736a9018eb48a215
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2024-07-19 17:37:40 +0530

    Backend hosted link added

--- frontend/src/services/Helper.js
+++ frontend/src/services/Helper.js
@@ -1,3 +1,2 @@
 //export const BACKEND_URL = "http://localhost:4002";
-export const BACKEND_URL =
-  "https://lost-found-web-application-e48c.vercel.app/";
+export const BACKEND_URL = "https://lost-found-web-application.onrender.com/";
