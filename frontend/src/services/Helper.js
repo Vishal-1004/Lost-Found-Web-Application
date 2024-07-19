be67a9fc0ba262bb311c28c643954d51146e1f23
VISHAL KUAMR YADAV <vishal100403@gmail.com>
2024-07-19 17:20:46 +0530

    Backend hosted

--- frontend/src/services/Helper.js
+++ frontend/src/services/Helper.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "http://localhost:4002";
+//export const BACKEND_URL = "http://localhost:4002";
+export const BACKEND_URL =
+  "https://lost-found-web-application-e48c.vercel.app/";
